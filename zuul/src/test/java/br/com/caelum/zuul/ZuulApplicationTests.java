2c24067effb76a22cac0eabdc422dcdce3be124a
unknown <tatisimoes86@gmail.com>
2022-03-21 17:07:30 -0300

    Implementando Gateway Zuul

---
+++ zuul/src/test/java/br/com/caelum/zuul/ZuulApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.caelum.zuul;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
